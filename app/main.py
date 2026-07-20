37c6fc3d12d4326a16462cea547fd70ba2f5d28c
Rejaul karim <rejaulkarim4740@gmail.com>
2026-07-20 13:37:27 +0600

    Allow localhost origin for CORS

--- app/main.py
+++ app/main.py
@@ -1,11 +1,28 @@
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.auth import router as auth_router
 from app.api.tasks import router as task_router
 from app.database.db import connection
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth_router)
 app.include_router(task_router)
 
